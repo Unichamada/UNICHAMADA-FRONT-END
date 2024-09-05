386755fa50ec3f96f48b5b5edc8cdc762a7f0726
gabrielgt5 <lucasgabrielgt758@gmail.com>
2024-09-04 23:55:57 -0400

    fix: corrigindo erros de componentes

--- src/app/(public)/evento/adiciona-evento/components/input-form.tsx
+++ src/app/(public)/evento/adiciona-evento/components/input-form.tsx
@@ -1,11 +1,11 @@
 import { Input } from "@/components/ui/input";
-
-i;
+import { Button, Popover } from "antd";
+import { Calendar } from "lucide-react";
 
 interface InputProps {
     placeholder: string;
 }
-export function InputForm({ placeholder }: InputProps) {
+export function Default({ placeholder }: InputProps) {
     return (
         <Input
             className="rounded-xl mt-1 block w-full px-3 py-2 bg-white border border-slate-300  text-sm shadow-sm placeholder-slate-400
@@ -15,31 +15,6 @@ export function InputForm({ placeholder }: InputProps) {
     );
 }
 
-export function DatePickerDemo() {
-    const [date, setDate] = React.useState<Date>();
-
-    return (
-        <Popover>
-            <PopoverTrigger asChild>
-                <Button
-                    variant={"outline"}
-                    className={cn(
-                        "w-[280px] justify-start text-left font-normal",
-                        !date && "text-muted-foreground",
-                    )}
-                >
-                    <CalendarIcon className="mr-2 h-4 w-4" />
-                    {date ? format(date, "PPP") : <span>Pick a date</span>}
-                </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-auto p-0">
-                <Calendar
-                    mode="single"
-                    selected={date}
-                    onSelect={setDate}
-                    initialFocus
-                />
-            </PopoverContent>
-        </Popover>
-    );
-}
+export const AppInput = {
+    Default,
+};
