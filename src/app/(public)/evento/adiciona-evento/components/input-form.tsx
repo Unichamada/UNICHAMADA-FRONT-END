5f9fbd66d906265450043971b660edda36c03791
Assis <assisneto013@gmail.com>
2024-09-05 00:48:05 -0400

    feat(nova tela): Adicionar nova tela detalhar evento

--- src/app/(public)/evento/adiciona-evento/components/input-form.tsx
+++ src/app/(public)/evento/adiciona-evento/components/input-form.tsx
@@ -1,7 +1,5 @@
 import { Input } from "@/components/ui/input";
 
-i;
-
 interface InputProps {
     placeholder: string;
 }
@@ -15,31 +13,3 @@ export function InputForm({ placeholder }: InputProps) {
     );
 }
 
-export function DatePickerDemo() {
-    const [date, setDate] = React.useState<Date>();
-
-    return (
-        <Popover>
-            <PopoverTrigger asChild>
-                <Button
-                    variant={"outline"}
-                    className={cn(
-                        "w-[280px] justify-start text-left font-normal",
-                        !date && "text-muted-foreground",
-                    )}
-                >
-                    <CalendarIcon className="mr-2 h-4 w-4" />
-                    {date ? format(date, "PPP") : <span>Pick a date</span>}
-                </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-auto p-0">
-                <Calendar
-                    mode="single"
-                    selected={date}
-                    onSelect={setDate}
-                    initialFocus
-                />
-            </PopoverContent>
-        </Popover>
-    );
-}
