7dcbbd4a0ffe044d7701b4408dbb70378ce11717
Gabriel Lucas <69332675+gabrielgt5@users.noreply.github.com>
2024-09-05 03:57:18 -0400

    Revert "Develop"

--- src/app/(public)/evento/page.tsx
+++ src/app/(public)/evento/page.tsx
@@ -1,18 +1,11 @@
 "use client";
 
-<<<<<<< HEAD
-=======
 import { CardEventos } from "@/components/card-eventos";
->>>>>>> e0c31b3f387ac9c690dc211353216ea6fa2e0a7a
 import SideMenu from "@/components/side-menu";
 import { Button } from "@/components/ui/button";
 import { Oi } from "next/font/google";
 import React from "react";
 import { useRouter } from "next/navigation";
-<<<<<<< HEAD
-import { Card } from "./adiciona-evento/components/card";
-=======
->>>>>>> e0c31b3f387ac9c690dc211353216ea6fa2e0a7a
 
 export default function TelaAdicionarEvento() {
     const router = useRouter();
@@ -33,30 +26,7 @@ export default function TelaAdicionarEvento() {
                 </Button>
                 <ul>
                     <li>
-<<<<<<< HEAD
-                        <Card
-                            titulo="Aula 01"
-                            items={[
-                                {
-                                    label: "Participantes",
-                                    icon: "UsersRound",
-                                    value: 100,
-                                },
-                                {
-                                    label: "Data do evento",
-                                    icon: "CalendarDays",
-                                    value: "10/10/2024",
-                                },
-                                {
-                                    label: "Hora do evento",
-                                    icon: "Clock",
-                                    value: "18:00",
-                                },
-                            ]}
-                        />
-=======
                         <CardEventos titulo="Aula 01" />
->>>>>>> e0c31b3f387ac9c690dc211353216ea6fa2e0a7a
                     </li>
                 </ul>
             </div>
