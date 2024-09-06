fd9bf5350108a7a739867982d108e7f8fe3f773f
alvessh <117163985+alvessh@users.noreply.github.com>
2024-09-06 10:08:30 -0400

    chore: melhorias no Dockerfile

--- src/app/(public)/evento/adiciona-evento/components/data-picker.tsx
+++ src/app/(public)/evento/adiciona-evento/components/data-picker.tsx
@@ -11,7 +11,7 @@ import {
     Popover,
     PopoverContent,
     PopoverTrigger,
-} from "@/components/ui/Popover";
+} from "@/components/ui/popover";
 
 export function DatePicker() {
     const [date, setDate] = React.useState<Date>();
