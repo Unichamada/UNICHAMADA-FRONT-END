cb9af9d3219b42befaf960fa66a455834e3ee443
magnojunior07 <magnojunior319@gmail.com>
2024-09-08 22:57:06 -0400

    feat: adiciona colunas de data e hora da presenca

--- src/services/Evento/dto/get-presenca-by-turma.dto.ts
+++ src/services/Evento/dto/get-presenca-by-turma.dto.ts
@@ -2,6 +2,8 @@ import { z } from "zod";
 
 export const getPresencaByTurmaDto = z.object({
     id: z.number(),
+    dataPresenca: z.string(),
+    horaPresenca: z.string(),
     pessoa: z.object({
         id: z.number(),
         nome: z.string(),
