178cf5f877324cd857f1dd97e6de34740836d520
magnojunior07 <magnojunior319@gmail.com>
2024-09-08 22:57:25 -0400

    refac: ajustes em geral nos dtos

--- src/services/Presenca/dto/create-presenca.dto.ts
+++ src/services/Presenca/dto/create-presenca.dto.ts
@@ -3,6 +3,8 @@ import { z } from "zod";
 const createPesencaDto = z.object({
     eventoId: z.number(),
     matricula: z.string(),
+    dataPresenca: z.date(),
+    horaPresenca: z.date(),
 });
 
 export type CreatePresencaDto = z.infer<typeof createPesencaDto>;
