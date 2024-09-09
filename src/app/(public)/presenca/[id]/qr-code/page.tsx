2045d573c05392699a3c286006d004c3db10b239
Gabriel Lucas <69332675+gabrielgt5@users.noreply.github.com>
2024-09-09 13:12:36 -0400

    refac(tela-qr-code): melhorando responsividade

--- src/app/(public)/presenca/[id]/qr-code/page.tsx
+++ src/app/(public)/presenca/[id]/qr-code/page.tsx
@@ -8,19 +8,20 @@ export default function QrCodePage() {
     const id = useParamId();
     const origin = window.location.origin;
     const urlCheckIn = `${origin}/presenca/${id}/check-in`;
+    const tamanhoDaLogo = 300;
 
     console.log(urlCheckIn);
 
     return (
-        <div className="flex bg-blue-200 items-center justify-center min-h-screen mx-auto p-4 h-screen ">
-            <div className="flex flex-col bg-blue-950 items-center justify-center p-6 gap-4 rounded-lg w-full h-full">
+        <div className="text-white flex items-center justify-center min-h-screen mx-auto p-4  bg-blue-950  ">
+            <div className="flex flex-col items-center justify-center p-6  gap-8 rounded-lg w-full h-full">
                 <Image
                     src="https://logo.uninassau.edu.br/img/svg/uninassau_n.svg"
-                    width={500}
-                    height={500}
+                    width={tamanhoDaLogo}
+                    height={tamanhoDaLogo}
                     alt="logo Uninassau azul com texto b"
                 />
-                <div className="bg-white p-4">
+                <div className="rounded-lg bg-white p-4">
                     <QRCode size={300} value={urlCheckIn} />
                 </div>
             </div>
