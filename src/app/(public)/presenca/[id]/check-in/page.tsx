8fbf25e25d0b95b3430ea71eb38c53f057bd8ded
Gabriel Lucas <69332675+gabrielgt5@users.noreply.github.com>
2024-09-09 13:23:15 -0400

    refac(tela-de-check-in): melhorando a responsividade

--- src/app/(public)/presenca/[id]/check-in/page.tsx
+++ src/app/(public)/presenca/[id]/check-in/page.tsx
@@ -56,15 +56,17 @@ export default function CheckIn() {
     });
 
     return (
-        <div className="flex items-center justify-center min-h-screen mx-auto p-4 h-screen  ">
-            <div className="flex flex-col bg-blue-950 items-center justify-center p-6 gap-4 rounded-lg w-96 h-5/6">
+        <div className=" flex items-center justify-center min-h-screen mx-auto p-4  bg-blue-950 ">
+            <div className="flex flex-col bg-blue-950 items-center justify-center p-6 gap-6 rounded-lg w-96 h-5/6">
                 <img
                     src="https://logo.uninassau.edu.br/img/svg/uninassau_n.svg"
                     width={240}
                     alt="logo Uninassau azul com texto b"
                 />
 
-                <p className="mx-auto text-xl text-white">Matricula</p>
+                <p className="mx-auto text-base font-medium text-white">
+                    Matricula
+                </p>
 
                 <Form {...form}>
                     <form
@@ -78,6 +80,8 @@ export default function CheckIn() {
                                 <FormItem>
                                     <FormControl>
                                         <Input
+                                            className="w-full"
+                                            type="number"
                                             placeholder="Insira sua matrícula para registrar presença"
                                             {...field}
                                         />
@@ -92,7 +96,7 @@ export default function CheckIn() {
                             className="w-full bg-blue-800 rounded-xl text-blue-50 hover:bg-blue-700 mb-8"
                             loading={isPending}
                         >
-                            Enviar
+                            Registrar presença
                         </Button>
                     </form>
                 </Form>
